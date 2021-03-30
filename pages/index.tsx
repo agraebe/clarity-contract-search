84915db326d0af816f9b91586bdd9d3804e19c7c
Alexander Graebe <agraebe@users.noreply.github.com>
2021-03-29 19:09:53 -0700

    feat: add redis caching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { Flex, Stack, Checkbox, Box, Text } from "@chakra-ui/react";
 import { useState } from "react";
+import bluebird from "bluebird";
+import redis from "redis";
 import { Footer } from "../components/footer/footer";
 import Contracts from "../components/contracts/contracts";
 import Header from "../components/header/header";
@@ -45,22 +47,38 @@ export default function Home({ contracts }: HomeProps) {
 }
 
 export async function getStaticProps() {
-  const res = await fetch(
-    "https://stacks-node-api.mainnet.stacks.co/extended/v1/tx/?limit=200&type=smart_contract"
-  );
-  const contracts = await res.json();
+  const apiUrl =
+    "https://stacks-node-api.mainnet.stacks.co/extended/v1/tx/?limit=200&type=smart_contract";
+  bluebird.promisifyAll(redis.RedisClient.prototype);
+  const cache = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+  });
+  let data = {};
 
-  if (!contracts) {
-    return {
-      notFound: true,
-    };
-  }
+  await cache.existsAsync("clarity-contracts").then(async (reply) => {
+    if (reply !== 1) {
+      // cache miss, need to fetch
+      data = await fetchData(apiUrl);
+      // expire after 24 hours
+      await cache.set("clarity-contracts", JSON.stringify(data), "EX", 86400);
+    } else {
+      // cache hit, will get data from redis
+      data = JSON.parse(await cache.getAsync("clarity-contracts"));
+    }
+  });
 
   return {
     props: {
-      contracts,
+      contracts: data,
     },
   };
+}
+
+async function fetchData(url) {
+  const query = await fetch(url);
+  return await query.json();
 }
 
 interface HomeProps {
